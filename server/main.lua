1fbb9152b4b5b73a21f7ff7bf336427b9f66f67d
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-14 18:25:43 -0600

    btc

--- server/main.lua
+++ server/main.lua
@@ -16,7 +16,7 @@ AddEventHandler("qb-recycle:server:getItem", function()
     local Player = QBCore.Functions.GetPlayer(src)
     for i = 1, math.random(2, 4), 1 do
         local randItem = ItemTable[math.random(1, #ItemTable)]
-        local amount = math.random(10, 25)
+        local amount = math.random(25, 45)
         Player.Functions.AddItem(randItem, amount)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[randItem], 'add')
         Citizen.Wait(500)
@@ -24,8 +24,8 @@ AddEventHandler("qb-recycle:server:getItem", function()
 
     local chance = math.random(1, 100)
     if chance < 7 then
-        Player.Functions.AddItem("cashroll", 5, false)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["cashroll"], "add")
+        Player.Functions.AddItem("markedbills", 4, false)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["markedbills"], "add")
     end
 
     local Luck = math.random(1, 10)
